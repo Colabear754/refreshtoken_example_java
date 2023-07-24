45f0a61457241299512b5641ba46a3b244d4b21d
Colabear754 <jky2406@gmail.com>
2023-07-24 15:45:14 +0900

    📝Swagger UI 설명 수정

--- src/main/java/com/colabear754/refreshtoken_example_java/swagger/SwaggerConfig.java
+++ src/main/java/com/colabear754/refreshtoken_example_java/swagger/SwaggerConfig.java
@@ -27,8 +27,8 @@ public class SwaggerConfig {
                                 .bearerFormat("JWT")))
                 .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
                 .info(new Info()
-                        .title("스프링시큐리티 + JWT 예제")
-                        .description("스프링시큐리티와 JWT를 이용한 사용자 인증 예제입니다.")
+                        .title("Refresh Token 예제")
+                        .description("기존 스프링시큐리티와 JWT를 이용한 사용자 인증 예제에 리프레시 토큰을 적용한 예제입니다.")
                         .version("1.0.0"));
     }
 
